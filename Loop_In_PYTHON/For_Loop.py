699dba3ab269b77532cb2cf1eb26f771940456bd
Amruta Bhujbal <62489086+AmrutaBhujbal02@users.noreply.github.com>
2020-05-23 23:05:14 +0530

    Add files via upload

---
+++ Loop_In_PYTHON/For_Loop.py
@@ -0,0 +1,9 @@
+
+#global i=20
+list_name=['tom','jack','johny']
+for name in list_name:
+    print(name+",welcome to python course!!!");
+print("Hope all of you enjoy it")
+
+
+#print("value of i is",format(i))
---
+++ Loop_In_PYTHON/Use_of_range().py
@@ -0,0 +1,8 @@
+
+numbers=list(range(1,15))
+print(numbers)
+
+
+list_num=list(range(1,10,2))
+for i in list_num:
+    print(i)
